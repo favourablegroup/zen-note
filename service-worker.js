a656c35d0b47ca3e53b06aa1f6e54e74af265d0f
favourablegroup <61370810+favourablegroup@users.noreply.github.com>
2023-10-25 16:31:30 +1300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'app-cache-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'styles.css',
+        'app.js'
+        // Add more files to cache as needed
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
